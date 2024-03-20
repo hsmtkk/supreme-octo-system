969f17e4c2c6b2521ca2908686f919da54a56870
Alice <you@example.com>
2024-03-20 16:13:11 +0900

    file upload code

--- front/pages/upload.py
+++ front/pages/upload.py
@@ -1,5 +1,17 @@
+import base64
 import os
+import requests
 import streamlit as st
 
-st.write("upload")
-st.write(os.environ)
+upload_api = os.environ["UPLOAD_API"]
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    encoded = base64.b64encode(bytes_data)
+    resp = requests.post(upload_api, data=encoded)
+    if resp.status_code == 200:
+        st.info("Upload done")
+    else:
+        st.error("Upload failed")
