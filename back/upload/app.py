6925c411b0ab77c6ae2e03e675f8b0d1b027617f
Alice <you@example.com>
2024-03-21 09:27:46 +0900

    separate upload and vector function

--- back/upload/app.py
+++ back/upload/app.py
@@ -4,14 +4,7 @@ import os
 import tempfile
 import boto3
 
-from langchain_community.document_loaders.pdf import UnstructuredPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_core.documents.base import Document
-
-vector_bucket = os.environ["VECTOR_BUCKET"]
-
+source_bucket = os.environ["SOURCE_BUCKET"]
 
 def lambda_handler(event, context) -> dict:
     print(f"{event=}")
@@ -20,46 +13,12 @@ def lambda_handler(event, context) -> dict:
     doc_bytes_encoded = event["body"]
     doc_bytes = base64.b64decode(doc_bytes_encoded)
 
-    raw_docs = load_document(doc_bytes)
-    docs = split_documents(raw_docs)
-    save_vector(docs)
-
-    return {"statusCode": 200, "body": json.dumps({"message": "ok"})}
-
-
-def load_document(doc_bytes: bytes) -> list[Document]:
-    with tempfile.NamedTemporaryFile() as temp_file:
+    with tempfile.NamedTemporaryFile(delete_on_close=False) as temp_file:
         temp_file.write(doc_bytes)
         temp_file.close()
+        upload_object(source_bucket, "doc.pdf", temp_file.name)
 
-        loader = UnstructuredPDFLoader(temp_file.name)
-        raw_docs = loader.load()
-        return raw_docs
-
-
-def split_documents(raw_docs: list[Document]) -> list[Document]:
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=50,
-        length_function=len,
-        is_separator_regex=False,
-    )
-    docs = text_splitter.split_documents(raw_docs)
-    return docs
-
-
-def save_vector(docs: list[Document]) -> None:
-    embeddings = OpenAIEmbeddings()
-    with tempfile.TemporaryDirectory() as temp_dir:
-        chroma = Chroma.from_documents(
-            documents=docs, embedding=embeddings, persist_directory="vector"
-        )
-        result = chroma.similarity_search("Abstract or summary")
-        print(f"{result=}")
-        file_name = "chroma.sqlite3"
-        db_path = os.path.join(temp_dir, file_name)
-        upload_object(vector_bucket, file_name, db_path)
-
+    return {"statusCode": 200, "body": json.dumps({"message": "ok"})}
 
 def upload_object(bucket: str, key: str, local_path: str) -> None:
     client = boto3.client("s3")
---
+++ back/vector/app.py
@@ -0,0 +1,67 @@
+import base64
+import json
+import os
+import tempfile
+import boto3
+
+from langchain_community.document_loaders.pdf import UnstructuredPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents.base import Document
+
+vector_bucket = os.environ["VECTOR_BUCKET"]
+
+
+def lambda_handler(event, context) -> dict:
+    print(f"{event=}")
+    print(f"{context=}")
+
+    doc_bytes_encoded = event["body"]
+    doc_bytes = base64.b64decode(doc_bytes_encoded)
+
+    raw_docs = load_document(doc_bytes)
+    docs = split_documents(raw_docs)
+    save_vector(docs)
+
+    return {"statusCode": 200, "body": json.dumps({"message": "ok"})}
+
+
+def load_document(doc_bytes: bytes) -> list[Document]:
+    with tempfile.NamedTemporaryFile() as temp_file:
+        temp_file.write(doc_bytes)
+        temp_file.close()
+
+        loader = UnstructuredPDFLoader(temp_file.name)
+        raw_docs = loader.load()
+        return raw_docs
+
+
+def split_documents(raw_docs: list[Document]) -> list[Document]:
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    docs = text_splitter.split_documents(raw_docs)
+    return docs
+
+
+def save_vector(docs: list[Document]) -> None:
+    embeddings = OpenAIEmbeddings()
+    with tempfile.TemporaryDirectory() as temp_dir:
+        chroma = Chroma.from_documents(
+            documents=docs, embedding=embeddings, persist_directory="vector"
+        )
+        result = chroma.similarity_search("Abstract or summary")
+        print(f"{result=}")
+        file_name = "chroma.sqlite3"
+        db_path = os.path.join(temp_dir, file_name)
+        upload_object(vector_bucket, file_name, db_path)
+
+
+def upload_object(bucket: str, key: str, local_path: str) -> None:
+    client = boto3.client("s3")
+    with open(local_path, "rb") as f:
+        client.put_object(Bucket=bucket, body=f, Key=key)
