79e63773a416254b26c2ec9a19d77f0c292136b8
Alice <you@example.com>
2024-03-21 13:26:03 +0900

    add secrets to store OpenAI API key

--- back/chat/app.py
+++ back/chat/app.py
@@ -1,16 +1,61 @@
+import boto3
 import json
 import os
+import tempfile
 
-def dump_envs() -> None:
-    for k,v in os.environ.items():
-        print(f"{k=}")
-        print(f"{v=}")
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain_openai import OpenAI
+
+vector_bucket = os.environ["VECTOR_BUCKET"]
+secret_arn = os.environ["SECRET_ARN"]
+
+
+def get_secrets() -> dict[str, str]:
+    client = boto3.client("secretsmanager")
+    resp = client.get_secret_value(SecretId=secret_arn)
+    secret_str = resp["SecretString"]
+    print(f"{secret_str=}")
+    return {"OPENAI_API_KEY": "placeholder"}
+
+
+secrets = get_secrets()
+
+tmp_dir = tempfile.mkdtemp()
+
+
+def load_vector(tmp_dir: str) -> None:
+    file_name = "chroma.sqlite3"
+    file_path = os.path.join(tmp_dir, file_name)
+    s3_client = boto3.client("s3")
+    resp = s3_client.get_object(Bucket=vector_bucket, Key=file_name)
+    content = resp["Body"].read()
+    with open(file_path, "wb") as f:
+        f.write(content)
+
+
+load_vector(tmp_dir)
+
+
+def init_qa_client():
+    embeddings = OpenAIEmbeddings()
+    chroma = Chroma(persist_directory=tmp_dir, embedding_function=embeddings)
+    retriever = chroma.as_retriever()
+    qa = RetrievalQA.from_llm(llm=OpenAI(), retriever=retriever)
+    return qa
+
+
+qa_client = init_qa_client()
+
 
 def lambda_handler(event, context) -> dict:
     print(f"{event=}")
     print(f"{context=}")
-    dump_envs()    
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"message":"ok"})
-    }
+
+    decoded = json.loads(event["body"])
+    question = decoded["question"]
+    answer = qa_client.invoke(question)
+    print(f"{answer=}")
+
+    return {"statusCode": 200, "body": json.dumps(answer)}
--- front/pages/chat.py
+++ front/pages/chat.py
@@ -1,5 +1,17 @@
 import os
+import requests
 import streamlit as st
 
-st.write("chat")
-st.write(os.environ)
+chat_api = os.environ["CHAT_API"]
+
+with st.form("form"):
+    question = st.text_area("Question")
+    submit_clicked = st.form_submit_button("Submit")
+
+if submit_clicked:
+    data = {
+        "question": question,
+    }
+    answer = requests.post(chat_api, json=data)
+    st.write(answer)
+    st.json(answer)
